aee4e10125777024f883d2d565f7c51106998524
mikolakran <mikolakran@mail.ru>
2023-01-20 14:04:29 +0300

    add display doctors.jsp for Users

--- src/main/java/com/web/repository/PositionDoctorJpaRepository.java
+++ src/main/java/com/web/repository/PositionDoctorJpaRepository.java
@@ -2,6 +2,12 @@ package com.web.repository;
 
 import com.web.entity.PositionDoctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 public interface PositionDoctorJpaRepository extends JpaRepository<PositionDoctor,Long> {
+    PositionDoctor findByPosition(String name);
+    @Query("SELECT t FROM PositionDoctor t LEFT JOIN FETCH t.doctors  where t.positionDoctorId = :id")
+    PositionDoctor getListDoctor(@Param("id") long id);
 }
